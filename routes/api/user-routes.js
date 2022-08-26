a28acfe3d65b210e0075e6ceeed43cb1e0b3fb27
Max Kottong <max.kottong@gmail.com>
2022-08-26 12:12:52 -0400

    Added comment functionality

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -14,10 +14,22 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
         include: [
             {
                 model: Post,
                 attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
             },
             {
                 model: Post,
@@ -25,11 +37,7 @@ router.get('/:id', (req, res) => {
                 through: Vote,
                 as: 'voted_posts'
             }
-        ],
-        attributes: { exclude: ['password'] },
-        where: {
-            id: req.params.id
-        }
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
